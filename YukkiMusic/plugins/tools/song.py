4631b495328a60bd62f5ddcc5a9d8a5ea5b6c959
aabb124 <127970495+aabb124@users.noreply.github.com>
2023-04-25 07:08:23 +0300

    Update song.py

--- YukkiMusic/plugins/tools/song.py
+++ YukkiMusic/plugins/tools/song.py
@@ -47,7 +47,7 @@ def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**✧ [𝖬𝗒 𝖲𝖳𝗎𝖿𝖿](t.me/UZZDD)**"
+        rep = f"**[𝖬𝗒 𝖲𝖳𝗎𝖿𝖿](t.me/UZZDD)**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
